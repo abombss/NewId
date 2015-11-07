a0ff9141387f3d75b005ad31d86de6421fea390d
Adam Tybor <adam.tybor@gmail.com>
2015-11-07 08:42:13 -0600

    Windows High Precision Tick Source

--- src/NewId.Tests/Provider_Specs.cs
+++ src/NewId.Tests/Provider_Specs.cs
@@ -1,3 +1,7 @@
+using System.CodeDom;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
 namespace MassTransit.NewIdTests
 {
     using System;
@@ -46,6 +50,120 @@ namespace MassTransit.NewIdTests
             var acceptableDelta = (long)(timeDelta.Ticks * 0.0001);
 
             Assert.Less(deltaTicks, acceptableDelta);
+        }
+    }
+
+    [TestFixture]
+    public class WindowsKernelFileTimeTickProvider_Specs
+    {
+        [Test, Explicit]
+        public void Should_Not_Drift()
+        {
+            var highResProvider = new WindowsKernelFileTimeTickProvider();
+            var stopwatchProvider = new StopwatchTickProvider();
+            
+            long highResStart = highResProvider.Ticks;
+            long stopwatchStart = stopwatchProvider.Ticks;
+
+            for (int i = 0; i < 10; i++)
+            {
+                // warmup
+                stopwatchStart = stopwatchProvider.Ticks;
+                highResStart = highResProvider.Ticks;
+            }
+
+            var start = new DateTime(highResProvider.Ticks, DateTimeKind.Utc);
+
+            var sw = Stopwatch.StartNew();
+            while (sw.Elapsed.TotalSeconds < 30)
+            {
+                var elapsedMilliseconds = sw.ElapsedMilliseconds;
+                var date = DateTime.UtcNow;
+                var highResDate = new DateTime(highResProvider.Ticks, DateTimeKind.Utc);
+                var stopwatchDate = new DateTime(stopwatchProvider.Ticks, DateTimeKind.Utc);
+
+                TimeSpan highResToStopwatch = highResDate - stopwatchDate;
+                TimeSpan highResToDate = highResDate - date;
+                TimeSpan stopwatchToDate = stopwatchDate - date;
+                
+                Console.WriteLine("{3:00000} Diff: HR-SW {0:0.000} ms, HR-D {1:0.000} ms, SW-D {2:0.000} ms", 
+                    highResToStopwatch.TotalMilliseconds,
+                    highResToDate.TotalMilliseconds,
+                    stopwatchToDate.TotalMilliseconds,
+                    elapsedMilliseconds);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        [Test, Explicit]
+        public void Should_Keep_Accurate_Time()
+        {
+            TimeSpan duration = TimeSpan.FromSeconds(5);
+            const int iterations = 10;
+            long[] highResStops = new long[iterations];
+            long[] stopwatchStops = new long[iterations];
+            long[] highResStarts = new long[iterations];
+            long[] stopwatchStarts = new long[iterations];
+
+            TimeSpan timeDelta = TimeSpan.FromTicks(duration.Ticks / iterations);
+            var highResProvider = new WindowsKernelFileTimeTickProvider();
+            var stopwatchProvider = new StopwatchTickProvider();
+
+            long highResStart = highResProvider.Ticks;
+            long stopwatchStart = stopwatchProvider.Ticks;
+
+            for (int i = 0; i < 10; i++)
+            {
+                // warmup
+                stopwatchStart = stopwatchProvider.Ticks;
+                highResStart = highResProvider.Ticks;                
+            }
+         
+
+            var highResTask = Task.Run(
+                () =>
+                {
+                    highResStart = highResProvider.Ticks;
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        highResStarts[i] = highResProvider.Ticks;
+                        Thread.Sleep(timeDelta);
+                        highResStops[i] = highResProvider.Ticks;
+
+                    }
+                });
+
+            Thread.Sleep(199);
+
+            var stopwatchTask = Task.Run(
+                () =>
+                {
+                    stopwatchStart = stopwatchProvider.Ticks;
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        stopwatchStarts[i] = stopwatchProvider.Ticks;
+                        Thread.Sleep(timeDelta);
+                        stopwatchStops[i] = stopwatchProvider.Ticks;
+
+                    }
+                });
+
+            Task.WhenAll(highResTask, stopwatchTask).Wait();
+
+            var acceptableDelta = (long)(timeDelta.Ticks);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                Console.WriteLine(
+                    "HR has a delta of {2:p} and total delta of {0:p}\nSW has a delta of {3:p} and total delta of {1:p}\n",
+                    (double)(Math.Abs(highResStops[i] - (highResStart + (i * timeDelta.Ticks))) - timeDelta.Ticks)
+                        / (double)acceptableDelta,
+                    (double)(Math.Abs(stopwatchStops[i] - (stopwatchStart + (i * timeDelta.Ticks))) - timeDelta.Ticks)
+                        / (double)acceptableDelta,
+                    Math.Abs((double)(Math.Abs(highResStops[i] - highResStarts[i]) - timeDelta.Ticks) / (double)acceptableDelta),
+                    Math.Abs((double)(Math.Abs(stopwatchStops[i] - stopwatchStarts[i]) - timeDelta.Ticks) / (double)acceptableDelta));
+            }
         }
     }
 }
---
+++ src/NewId/NewIdProviders/WindowsKernelFileTimeTickProvider.cs
@@ -0,0 +1,131 @@
+﻿namespace MassTransit.NewIdProviders
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    public sealed class WindowsKernelFileTimeTickProvider : ITickProvider
+    {
+        public long Ticks
+        {
+            get { return HighResolutionDateTime.UtcNow.Ticks; }
+        }
+    }
+
+    [SuppressUnmanagedCodeSecurity]
+    static class HighResolutionDateTime
+    {
+        abstract class DateTimeProvider
+        {
+            public abstract DateTime UtcNow { get; }
+
+            public abstract long FileTimeUtcNow { get; }
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        sealed class NativeDateTime : DateTimeProvider
+        {
+            [SuppressUnmanagedCodeSecurity]
+            [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+            internal static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+            static readonly Lazy<bool> IsApiAvailable = new Lazy<bool>(
+                () =>
+                {
+                    try
+                    {
+                        long filetime;
+                        GetSystemTimePreciseAsFileTime(out filetime);
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                });
+
+            public static bool IsAvailable
+            {
+                get { return IsApiAvailable.Value; }
+            }
+
+            public override long FileTimeUtcNow
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                [SuppressUnmanagedCodeSecurity]
+                get
+                {
+                    long time;
+                    GetSystemTimePreciseAsFileTime(out time);
+                    return time;
+                }
+            }
+
+            public override DateTime UtcNow
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                [SuppressUnmanagedCodeSecurity]
+                get
+                {
+                    long time;
+                    GetSystemTimePreciseAsFileTime(out time);
+                    return DateTime.FromFileTimeUtc(time);
+                }
+            }
+        }
+
+        sealed class ManagedDateTime : DateTimeProvider
+        {
+            public override DateTime UtcNow
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)] get { return DateTime.UtcNow; }
+            }
+
+            public override long FileTimeUtcNow
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)] get { return DateTime.UtcNow.ToFileTimeUtc(); }
+            }
+        }
+
+        static readonly DateTimeProvider Provider;
+        static readonly bool _isHighRes;
+
+        public static bool IsHighPrecision
+        {
+            get { return _isHighRes; }
+        }
+
+        public static long FileTimeUtcNow
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            [SuppressUnmanagedCodeSecurity]
+            get
+            {
+                return Provider.FileTimeUtcNow;
+            }
+        }
+
+        public static DateTime UtcNow
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            [SuppressUnmanagedCodeSecurity]
+            get
+            {
+                if (_isHighRes)
+                {
+                    long time;
+                    NativeDateTime.GetSystemTimePreciseAsFileTime(out time);
+                    return DateTime.FromFileTimeUtc(time);                    
+                }
+                return Provider.UtcNow;
+            }
+        }
+
+        static HighResolutionDateTime()
+        {
+            Provider = NativeDateTime.IsAvailable ? (DateTimeProvider)new NativeDateTime() : new ManagedDateTime();
+            _isHighRes = Provider is NativeDateTime;
+        }
+    }
+}
